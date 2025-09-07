60d0fd9a1a09747fa0f55b74c9052eabc5eea6d8
laililula <alsrbrr12337@gmail.com>
2025-09-07 13:42:28 +0900

    modified:   css/style.css 	modified:   index.html 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,18 @@
-const text = "개발자와 비개발자가 같이 성장하는 즐거움 with Google";
+const text = "Google Developer Group on Student<br>Dong-A University";
 const typingText = document.getElementById("typing-text");
 
 let i = 0;
 
 function typing() {
   if (i < text.length) {
-    typingText.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typing, 100); // 글자가 하나씩 찍히는 속도 (100ms)
+    if (text.substring(i, i + 4) === "<br>") {
+      typingText.innerHTML += "<br>"; // 줄바꿈 태그는 그대로 추가
+      i += 4;
+    } else {
+      typingText.innerHTML += text.charAt(i); // 일반 문자 추가
+      i++;
+    }
+    setTimeout(typing, 60); // 타이핑 속도
   }
 }
 
